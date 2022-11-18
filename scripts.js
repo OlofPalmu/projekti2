fc4695bdfa33d7cbba7793cb169b184f3190d1f5
OlofPalmu <ville.palmu@student.laurea.fi>
2022-11-18 11:43:22 +0200

    alkutoimintaa

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
+function loadXMLFile() {
+    var url = "https://www.finnkino.fi/xml/Schedule/";
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200){
+            parseXML(this);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function parseXML (xml) {
+    var xmlData = xml.responseXML;
+    var table = "<table>";
+    var x = xmlData.getElementsByTagName("Title");
+    for (i=0; i < 10; i++){
+        table += "<tr><td>" + x[i].childNodes[0].nodeValue + "</tr></td>"
+    }
+    table += "</table>";
+    document.getElementById("results").innerHTML = table;
+}
 
